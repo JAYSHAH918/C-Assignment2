4f627615571af8f5e3a0d470997767b7758ce0f6
JAYSHAH918 <59958579+JAYSHAH918@users.noreply.github.com>
2020-01-24 22:43:37 +0530

    Add files via upload

---
+++ .github/workflows/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        public abstract class Employee
+        {
+            String Id, Name, Designation, Experience, AnnualSalary, JoiningDate;
+            public abstract void salary();
+        }
+
+        public class HR : Employee
+        {
+            public override void salary()
+            {
+                int basic = 8000;
+                String exp = Console.ReadLine();
+                int exper = Convert.ToInt16(exp);
+                
+                int total = (basic + (exper * 1000));
+                Console.WriteLine("Salary of HR is" + total);
+                Console.WriteLine(" ");
+            }
+        }
+
+
+        public class Developer : Employee
+        {
+            public override void salary()
+            {
+                int basic = 8000;
+                String exp = Console.ReadLine();
+                int exper = Convert.ToInt16(exp);
+               
+                int total = (basic + (exper * 2000));
+                Console.WriteLine("Salary of Developer is" + total);
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Employee e;
+
+            e = new HR();
+            Console.WriteLine("Enter experiece year of HR");
+            e.salary();
+
+
+            e = new Developer();
+            Console.WriteLine("Enter experiece year of Developer");
+            e.salary();
+        }
+    }
+}
